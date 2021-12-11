fd356e6c889e41fa5fa9cf44c75a5232a3e2f779
Alex Tomé da Silva <alextsilva86@gmail.com>
2021-12-11 18:59:16 -0300

    Manutenção Código
    Remoção details, aviso de apagar e arquivos não utilizados

--- GastosContas/Areas/Identity/Data/GastosContasContext.cs
+++ GastosContas/Areas/Identity/Data/GastosContasContext.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using GastosContas.Areas.Identity.Data;
+﻿using GastosContas.Areas.Identity.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
--- GastosContas/Areas/Identity/IdentityHostingStartup.cs
+++ GastosContas/Areas/Identity/IdentityHostingStartup.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using GastosContas.Areas.Identity.Data;
+﻿using GastosContas.Areas.Identity.Data;
 using GastosContas.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
