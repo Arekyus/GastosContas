03c70079d28686386f8f170b73e39e79ecb9162b
Alex Tomé da Silva <alextsilva86@gmail.com>
2021-12-15 08:32:16 -0300

    API Email
    Configurado API envio de email para confirmação de cadastro e recuperação de senha

--- GastosContas/Services/AuthMessageSenderOptions.cs
+++ GastosContas/Services/AuthMessageSenderOptions.cs
@@ -1,4 +1,9 @@
-﻿namespace GastosContas.Services
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Threading.Tasks;
+
+namespace GastosContas.Services
 {
     public class AuthMessageSenderOptions
     {
