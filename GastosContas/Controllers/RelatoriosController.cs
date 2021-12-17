37eb174e866e227e98e4b2f056a3c5f726e13ced
Alex Tomé da Silva <alextsilva86@gmail.com>
2021-12-17 04:25:21 -0300

    Chats Informando os Dias
    Chats informando os dias mas não puxando ainda os dados da api

--- GastosContas/Controllers/RelatoriosController.cs
+++ GastosContas/Controllers/RelatoriosController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
 
 namespace GastosContas.Controllers
 {
@@ -9,6 +11,21 @@ namespace GastosContas.Controllers
         [Authorize]
         public IActionResult Index()
         {
+            var mes = DateTime.Today.Month;
+            var ano = DateTime.Today.Year;
+            var quantidadeDias = DateTime.DaysInMonth(ano, mes);
+
+            var dias = new List<int>();
+
+            for (var dia = 1; dia <= quantidadeDias; dia++)
+            {
+                dias.Add(dia);  
+            }
+
+            ViewBag.Dias = dias;
+            ViewBag.Mes = mes;
+            ViewBag.Ano = ano;
+
             return View();
         }
 
